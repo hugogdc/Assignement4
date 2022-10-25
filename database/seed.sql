9198c0481517b077c7d40308b869ef6db4df75a7
hugogdc <hugo.decolombe@gmail.com>
2022-10-24 21:05:56 -0700

    commit

--- database/seed.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `assignement3`.`company` (`idcompany`, `name`, `numberEmployee`, `value`) VALUES ('01', 'Microsoft', '500', '100000000000');
-INSERT INTO `assignement3`.`avgpaycheck` (`idavgpaycheck`, `salary`, `bonus`, `total`) VALUES ('01', '2000', '200', '2200');
-INSERT INTO `assignement3`.`job` (`idjob`, `jobname`, `idavgpaycheck`, `idcompany`) VALUES ('01', 'Engineer', '01', '01');
-INSERT INTO `assignement3`.`persons` (`idpersons`, `firstname`, `lastname`, `idjob`) VALUES ('01', 'John', 'Doe', '01');
---
+++ queries.js
@@ -0,0 +1,70 @@
+
+const Pool = require('pg').Pool
+const db = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'assignement4',
+    password: 'root',
+    port: 5432,
+})
+
+const getUsers = (request, response) => {
+    db.query('SELECT * FROM assignement4.users ORDER BY id ASC', (error, results) => {
+        if (error) console.log(error);
+        response.status(200).json(results.rows)
+    })
+}
+const getUserById = (request, response) => {
+    const id = parseInt(request.params.id)
+    db.query('SELECT * FROM assignement4.users WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+const createUser = (request, response) => {
+    const {
+        name,
+        email
+    } = request.body
+    db.query('INSERT INTO assignement4.users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(`User added with ID: ${results.insertId}`)
+    })
+}
+const updateUser = (request, response) => {
+    const id = parseInt(request.params.id)
+    const {
+        name,
+        email
+    } = request.body
+    db.query(
+        'UPDATE assignement4.users SET name = $1, email = $2 WHERE id = $3',
+        [name, email, id],
+        (error, results) => {
+            if (error) {
+                throw error
+            }
+            response.status(200).send(`User modified with ID: ${id}`)
+        }
+    )
+}
+const deleteUser = (request, response) => {
+    const id = parseInt(request.params.id)
+    db.query('DELETE FROM assignement4.users WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).send(`User deleted with ID: ${id}`)
+    })
+}
+module.exports = {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+}
